c66263c2eb1f90a94df931e98fbbbf34f20378f5
第一システム開発部 佐々木　正太郎 <shotaro-sasaki@dac.co.jp>
2017-11-15 10:24:07 +0900

    comment

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"time"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
@@ -19,21 +21,21 @@ func main() {
 	svc := sqs.New(s, aws.NewConfig().WithEndpoint("http://localhost:9324/queue/").WithRegion("ap-northeast-1"))
 
 	bodyTmpl := `{
-    "replyToken": "%v%v",
-    "type": "message",
-    "timestamp": 1462629479859,
-    "source": {
-      "type": "user",
-      "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
-    },
-    "message": {
-      "id": "325708",
-      "type": "text",
-      "text": "Hello, world"
-    }
-  }`
+      "replyToken": "%v%v",
+      "type": "message",
+      "timestamp": 1462629479859,
+      "source": {
+        "type": "user",
+        "userId": "U1234567890abcdefgh1234567890abcd"
+      },
+      "message": {
+        "id": "325708",
+        "type": "text",
+        "text": "Hello, world"
+      }
+    }`
 
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 100; i++ {
 		fmt.Println(fmt.Sprintf("No.%v", i))
 		res, err := svc.SendMessageBatch(&sqs.SendMessageBatchInput{
 			QueueUrl: aws.String("http://localhost:9324/queue/local_line_messages"),
@@ -43,7 +45,7 @@ func main() {
 					Id: aws.String(fmt.Sprintf("%v%v", "A", i)),
 					MessageAttributes: map[string]*sqs.MessageAttributeValue{
 						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("736afbdc388752eb")},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("1234abcd5678efgh")},
 						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("1")},
 					},
 					MessageBody: aws.String(fmt.Sprintf(bodyTmpl, "A", i)),
@@ -53,7 +55,7 @@ func main() {
 					Id: aws.String(fmt.Sprintf("%v%v", "B", i)),
 					MessageAttributes: map[string]*sqs.MessageAttributeValue{
 						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("736afbdc388752eb")},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("1234abcd5678efgh")},
 						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("1")},
 					},
 					MessageBody: aws.String(fmt.Sprintf(bodyTmpl, "B", i)),
@@ -63,7 +65,7 @@ func main() {
 					Id: aws.String(fmt.Sprintf("%v%v", "C", i)),
 					MessageAttributes: map[string]*sqs.MessageAttributeValue{
 						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("736afbdc388752eb")},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("1234abcd5678efgh")},
 						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("1")},
 					},
 					MessageBody: aws.String(fmt.Sprintf(bodyTmpl, "C", i)),
@@ -73,7 +75,7 @@ func main() {
 					Id: aws.String(fmt.Sprintf("%v%v", "D", i)),
 					MessageAttributes: map[string]*sqs.MessageAttributeValue{
 						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("736afbdc388752eb")},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("1234abcd5678efgh")},
 						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("1")},
 					},
 					MessageBody: aws.String(fmt.Sprintf(bodyTmpl, "D", i)),
@@ -83,7 +85,7 @@ func main() {
 					Id: aws.String(fmt.Sprintf("%v%v", "E", i)),
 					MessageAttributes: map[string]*sqs.MessageAttributeValue{
 						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("736afbdc388752eb")},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("1234abcd5678efgh")},
 						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("1")},
 					},
 					MessageBody: aws.String(fmt.Sprintf(bodyTmpl, "E", i)),
@@ -93,7 +95,7 @@ func main() {
 					Id: aws.String(fmt.Sprintf("%v%v", "F", i)),
 					MessageAttributes: map[string]*sqs.MessageAttributeValue{
 						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("736afbdc388752eb")},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("1234abcd5678efgh")},
 						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("1")},
 					},
 					MessageBody: aws.String(fmt.Sprintf(bodyTmpl, "F", i)),
@@ -103,7 +105,7 @@ func main() {
 					Id: aws.String(fmt.Sprintf("%v%v", "G", i)),
 					MessageAttributes: map[string]*sqs.MessageAttributeValue{
 						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("736afbdc388752eb")},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("1234abcd5678efgh")},
 						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("1")},
 					},
 					MessageBody: aws.String(fmt.Sprintf(bodyTmpl, "G", i)),
@@ -113,7 +115,7 @@ func main() {
 					Id: aws.String(fmt.Sprintf("%v%v", "H", i)),
 					MessageAttributes: map[string]*sqs.MessageAttributeValue{
 						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("736afbdc388752eb")},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("1234abcd5678efgh")},
 						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("1")},
 					},
 					MessageBody: aws.String(fmt.Sprintf(bodyTmpl, "H", i)),
@@ -123,7 +125,7 @@ func main() {
 					Id: aws.String(fmt.Sprintf("%v%v", "I", i)),
 					MessageAttributes: map[string]*sqs.MessageAttributeValue{
 						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("736afbdc388752eb")},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("1234abcd5678efgh")},
 						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("1")},
 					},
 					MessageBody: aws.String(fmt.Sprintf(bodyTmpl, "I", i)),
@@ -133,7 +135,7 @@ func main() {
 					Id: aws.String(fmt.Sprintf("%v%v", "J", i)),
 					MessageAttributes: map[string]*sqs.MessageAttributeValue{
 						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("736afbdc388752eb")},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String("1234abcd5678efgh")},
 						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("1")},
 					},
 					MessageBody: aws.String(fmt.Sprintf(bodyTmpl, "J", i)),
@@ -146,7 +148,17 @@ func main() {
 		if res == nil {
 			panic("res is nil")
 		}
-		//time.Sleep(10 * time.Millisecond)
+
+		// 秒間１０メッセージ配信用（ループカウント１００と組み合わせて、トータル１０００メッセージ配信）
+		time.Sleep(1 * time.Second)
+		// 秒間２０メッセージ配信用（ループカウント２００と組み合わせて、トータル２０００メッセージ配信）
+		//time.Sleep(500 * time.Millisecond)
+		// 秒間４０メッセージ配信用（ループカウント４００と組み合わせて、トータル４０００メッセージ配信）
+		//time.Sleep(250 * time.Millisecond)
+		// 秒間１００メッセージ配信用（ループカウント１０００と組み合わせて、トータル１００００メッセージ配信）
+		//time.Sleep(100 * time.Millisecond)
+		// 秒間５００メッセージ配信用（ループカウント５０００と組み合わせて、トータル５００００メッセージ配信）
+		//time.Sleep(20 * time.Millisecond)
 		fmt.Println("Next!")
 	}
 }
