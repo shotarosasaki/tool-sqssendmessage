93137154bbfbb4e2448435b41422d1d1c28014d9
第一システム開発部 佐々木　正太郎 <shotaro-sasaki@dac.co.jp>
2017-11-27 18:28:43 +0900

    作り直し中

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 
 	"time"
 
@@ -72,9 +73,8 @@ func main() {
 	argACID := flag.String("acid", "1234abcd5678efgh", "ACID")
 	argUserID := flag.String("userID", "U1234567890abcdefgh1234567890abcd", "ユーザID")
 	argQueueName := flag.String("queueName", "local_line_messages", "SQSキュー名")
-	argLoopCount := flag.Int("loopCount", 1, "ループカウント")
-	argDelay := flag.String("delay", "1s", "１ループ毎の遅延時間（「500ms」のようなtime.Duration形式）")
-	argEntryCount := flag.Int("entryCount", 1, "１ループ毎の送信メッセージ数")
+	argThroughput := flag.Int("throughput", 10, "秒間スループット")
+	argDuration := flag.Int("duration", 30, "継続時間（単位：秒）")
 
 	argEndpoint := flag.String("endpoint", "http://localhost:9324/queue/", "SQSエンドポイント(ローカルでElasticMQ起動した際のエンドポイント指定用)")
 
@@ -82,9 +82,8 @@ func main() {
 
 	fmt.Printf("[%v][ACID:%v]ARGS(userID:%v)\n", time.Now().Format(tformat), *argACID, *argUserID)
 	fmt.Printf("[%v][ACID:%v]ARGS(queueName:%v)\n", time.Now().Format(tformat), *argACID, *argQueueName)
-	fmt.Printf("[%v][ACID:%v]ARGS(loopCount:%v)\n", time.Now().Format(tformat), *argACID, *argLoopCount)
-	fmt.Printf("[%v][ACID:%v]ARGS(delay:%v)\n", time.Now().Format(tformat), *argACID, *argDelay)
-	fmt.Printf("[%v][ACID:%v]ARGS(entryCount:%v)\n", time.Now().Format(tformat), *argACID, *argEntryCount)
+	fmt.Printf("[%v][ACID:%v]ARGS(throughput:%v)\n", time.Now().Format(tformat), *argACID, *argThroughput)
+	fmt.Printf("[%v][ACID:%v]ARGS(duration:%v)\n", time.Now().Format(tformat), *argACID, *argDuration)
 
 	fmt.Printf("[%v][ACID:%v]ARGS(endpoint:%v)\n", time.Now().Format(tformat), *argACID, *argEndpoint)
 
@@ -104,59 +103,68 @@ func main() {
 	fmt.Printf("[%v][ACID:%v]START\n", time.Now().Format(tformat), *argACID)
 	fmt.Printf("[%v][ACID:%v]queueUrl:%v\n", time.Now().Format(tformat), *argACID, *queueUrl)
 
-	// ディレイかけながら複数回のSQSバッチ送信を行う
-	for i := 0; i < *argLoopCount; i++ {
-		fmt.Printf("[%v][ACID:%v]No.%v\n", time.Now().Format(tformat), *argACID, i)
+	loopCount := *argThroughput / 10
+	fmt.Printf("[%v][ACID:%v]loopCount:%v\n", time.Now().Format(tformat), *argACID, loopCount)
 
-		var entries []*sqs.SendMessageBatchRequestEntry
+	// 1秒ごとに指定スループットのメッセージをSQSに送信
+	for k := 0; k < *argDuration; k++ {
+		var wg sync.WaitGroup
 
-		// SQSへのバッチ送信に渡すエントリーを指定件数分、生成
-		for j := 0; j < *argEntryCount; j++ {
+		// 1回のバッチ送信で１０メッセージまで同時に送信可能
+		// 例えば、指定スループットが秒間２００メッセージとすると、以下のループカウントは２００÷１０＝２０回となる（各ループはGoroutine実行のため非同期）
+		for i := 0; i < loopCount; i++ {
+			fmt.Printf("[%v][ACID:%v]No.%v-%v\n", time.Now().Format(tformat), *argACID, k, i)
 
-			// LINE-API[Push](https://developers.line.me/ja/docs/messaging-api/reference/#anchor-0c00cb0f42b970892f7c3382f92620dca5a110fc)のメッセージ形式に依存
-			pushStruct := &struct {
-				To       string            `json:"to"`
-				Messages []linebot.Message `json:"messages"`
-			}{
-				To:       *argUserID,
-				Messages: []linebot.Message{linebot.NewTextMessage(fmt.Sprintf("SQS投入負荷ツール：No.%v-%v", i, j))},
+			var entries []*sqs.SendMessageBatchRequestEntry
+
+			// SQSへのバッチ送信に渡すエントリーを指定件数分、生成
+			for j := 0; j < 10; j++ {
+
+				// LINE-API[Push](https://developers.line.me/ja/docs/messaging-api/reference/#anchor-0c00cb0f42b970892f7c3382f92620dca5a110fc)のメッセージ形式に依存
+				pushStruct := &struct {
+					To       string            `json:"to"`
+					Messages []linebot.Message `json:"messages"`
+				}{
+					To:       *argUserID,
+					Messages: []linebot.Message{linebot.NewTextMessage(fmt.Sprintf("SQS投入負荷ツール：No.%v-%v-%v", k, i, j))},
+				}
+				btEvent, err := json.Marshal(pushStruct)
+				if err != nil {
+					panic(err)
+				}
+				bodyTmpl := string(btEvent)
+
+				entries = append(entries, &sqs.SendMessageBatchRequestEntry{
+					Id: aws.String(fmt.Sprintf("%v%v%v", k, i, j)),
+					MessageAttributes: map[string]*sqs.MessageAttributeValue{
+						"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
+						"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(*argACID)},
+						"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("2")},
+					},
+					MessageBody: aws.String(bodyTmpl),
+				})
 			}
-			btEvent, err := json.Marshal(pushStruct)
-			if err != nil {
-				panic(err)
-			}
-			bodyTmpl := string(btEvent)
 
-			entries = append(entries, &sqs.SendMessageBatchRequestEntry{
-				Id: aws.String(fmt.Sprintf("%v%v", i, j)),
-				MessageAttributes: map[string]*sqs.MessageAttributeValue{
-					"uniqueMessageID": &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(uuid.NewV4().String())},
-					"acid":            &sqs.MessageAttributeValue{DataType: aws.String("String"), StringValue: aws.String(*argACID)},
-					"sendType":        &sqs.MessageAttributeValue{DataType: aws.String("Number"), StringValue: aws.String("2")},
-				},
-				MessageBody: aws.String(bodyTmpl),
-			})
+			wg.Add(1)
+			go func(wg *sync.WaitGroup) {
+				defer wg.Done()
+				res, err := svc.SendMessageBatch(&sqs.SendMessageBatchInput{
+					QueueUrl: queueUrl,
+					Entries:  entries,
+				})
+				if err != nil {
+					fmt.Printf("[%v][ACID:%v]SendMessageBatch Error:%#v\n", time.Now().Format(tformat), *argACID, err.Error())
+					panic(err)
+				}
+				if res == nil {
+					panic("res is nil")
+				}
+			}(&wg)
 		}
 
-		res, err := svc.SendMessageBatch(&sqs.SendMessageBatchInput{
-			QueueUrl: queueUrl,
-			Entries:  entries,
-		})
-		if err != nil {
-			fmt.Printf("[%v][ACID:%v]SendMessageBatch Error:%#v\n", time.Now().Format(tformat), *argACID, err.Error())
-			panic(err)
-		}
-		if res == nil {
-			panic("res is nil")
-		}
+		wg.Wait()
 
-		delay, err := time.ParseDuration(*argDelay)
-		if err != nil {
-			fmt.Printf("[%v][ACID:%v]ParseDuration Error:%#v\n", time.Now().Format(tformat), *argACID, err.Error())
-			panic(err)
-		}
-
-		time.Sleep(delay)
+		time.Sleep(1 * time.Second)
 	}
 
 	fmt.Printf("[%v][ACID:%v]END\n", time.Now().Format(tformat), *argACID)
